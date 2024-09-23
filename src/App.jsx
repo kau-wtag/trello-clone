207b518495d6c0a80370bca5b65df203b0f7d565
Kaium Uddin <kaium.uddin@welldev.io>
2024-09-23 15:12:52 +0600

    remove tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-// App.jsx
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex">
-      <nav role="navigation" className="w-full bg-blue-600 p-4 text-white">
-        Navbar
-      </nav>
-      <aside role="complementary" className="w-64 bg-gray-300 p-4">
-        Sidebar
-      </aside>
-      <main role="main" className="flex-grow p-4 bg-white">
-        Main Content
-      </main>
+    <div className="min-h-screen bg-gray-100 flex flex-col">
+      <nav className="w-full bg-blue-600 p-4 text-white">Navbar</nav>
+      <div className="flex flex-grow">
+        <aside className="w-64 bg-gray-300 p-4">Sidebar</aside>
+        <main className="flex-grow p-4 bg-white">Main Content</main>
+      </div>
     </div>
   );
 };
