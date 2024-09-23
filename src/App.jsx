b3336db6e62640530a2c51aa0008f52833bc0d57
Kaium Uddin <kaium.uddin@welldev.io>
2024-09-23 11:13:31 +0600

    install testing packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+// App.jsx
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex">
-      {/* Navbar */}
-      {/* Sidebar  */}
-      {/* Main */}
+      <nav role="navigation" className="w-full bg-blue-600 p-4 text-white">
+        Navbar
+      </nav>
+      <aside role="complementary" className="w-64 bg-gray-300 p-4">
+        Sidebar
+      </aside>
+      <main role="main" className="flex-grow p-4 bg-white">
+        Main Content
+      </main>
     </div>
   );
 };
