301ccee9c38fe75488ca8ea02deafedd9ace1114
Kaium Uddin <kaium.uddin@welldev.io>
2024-09-25 15:23:43 +0600

    text-left

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -23,7 +23,7 @@ const Board = () => {
 
   return (
     <DragDropContext onDragEnd={handleDragEnd}>
-      <h2 className="text-2xl font-bold mb-4 text-center text-gray-800">
+      <h2 className="text-2xl font-bold mb-4 text-gray-800">
         {board.name}
       </h2>
       <SectionContainer sections={board.sections} />
--- src/components/SectionContainer.jsx
+++ src/components/SectionContainer.jsx
@@ -2,7 +2,7 @@ import Section from "./Section";
 
 const SectionContainer = ({ sections }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-5 p-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-5">
       {sections.map((section, index) => (
         <Section key={section.id} section={section} index={index} />
       ))}
