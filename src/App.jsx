cf0697e7fa606d6f92b0ec96d991bab2a678a4cc
Kaium Uddin <kaium.uddin@welldev.io>
2024-09-30 11:33:40 +0600

    add absolute import

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import Navbar from "src/components/Navbar";
 
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col">
-      <nav className="w-full bg-blue-600 p-4 text-white">
-      </nav>
-      <Navbar/>
+      <nav className="w-full bg-blue-600 p-4 text-white">Navbar</nav>
       <div className="flex flex-grow">
         <aside className="w-64 bg-gray-300 p-4">Sidebar</aside>
         <main className="flex-grow p-4 bg-white">Main Content</main>
